a22018cf3caa6268127e3dba98d2530c8bef4b71
TugsjargalGantumur <tugsjargalgantumur@gmail.com>
2024-10-07 11:58:36 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+import { Hero } from "./components";
+import { Header } from "./components/header/Header";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Header />
+      <Hero />
+    </div>
+  );
 };
---
+++ src/components/hero/Hero.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Hero.css";
+export const Hero = () => {
+  return (
+    <div id="hero-container">
+      <div id="hero-text">
+        <h1>Khuvsgul Lake</h1>
+        <h2>TRIP</h2>
+      </div>
+
+      <div id="hero-image">
+        <img src="hero-images/hushuu.png" alt="Nature" />
+        <img src="hero-images/hiid.png" alt="Nature" />
+        <img src="hero-images/gernuud.png" alt="Nature" />
+        <img src="hero-images/mod.png" alt="Nature" />
+      </div>
+    </div>
+  );
+};

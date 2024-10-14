8b5093744cc9d8984d0e3937d5cf4b33f2797305
TugsjargalGantumur <tugsjargalgantumur@gmail.com>
2024-10-14 11:44:25 -0500

    phone

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./Hero.css";
+
 export const Hero = () => {
   return (
     <div id="hero-container">
-      <div id="hero-text">
-        <h1>Khuvsgul Lake</h1>
-        <h2>TRIP</h2>
+      <div>
+        <span id="hero-title">Khuvsgul Lake</span>
       </div>
 
-      <div id="hero-image">
+      <div>
+        <span id="hero-subtitle">Trip</span>
+      </div>
+
+      <div id="hero-image-container">
         <img src="hero-images/hushuu.png" alt="Nature" />
         <img src="hero-images/hiid.png" alt="Nature" />
         <img src="hero-images/gernuud.png" alt="Nature" />
